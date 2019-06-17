a8dddbb828993c910bce429aca8e7c9b3af8bb4e
JonariW <48421070+JonariW@users.noreply.github.com>
2019-06-17 13:53:20 -0400

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,18 +1,21 @@
 import java.util.*;
+import java.beans.*;
 import java.io.*;
 public class Student implements Serializable {
-	private String firstName; 
-    private String birthYear; 
-    private String yearInCollege;
-    private String cwid;
-    private static String binary;
-    private static Student deserialStudent;
+	
+	protected String firstName; 
+    protected String birthYear; 
+    protected String yearsInCollege;
+    protected String cwid;
+    protected static String binaryNumber;
+    protected static Student deserializedBinaryStudent;
+    protected static Student deserializedXMLStudent;
     
     
-	public Student(String firstName, String birthYear, String yearInCollege, String cwid) { 
+	public Student(String firstName, String birthYear, String yearsInCollege, String cwid) { 
 	        this.firstName = firstName; 
 	        this.birthYear = birthYear;
-	        this.yearInCollege = yearInCollege;
+	        this.yearsInCollege = yearsInCollege;
 	        this.cwid = cwid;
 	}
 	
@@ -21,7 +24,7 @@ public class Student implements Serializable {
 			ByteArrayOutputStream byteOutStream = new ByteArrayOutputStream();
 			ObjectOutputStream objOutStream = new ObjectOutputStream(byteOutStream);
 			objOutStream.writeObject(student);
-			binary = Base64.getEncoder().encodeToString(byteOutStream.toByteArray());
+			binaryNumber = Base64.getEncoder().encodeToString(byteOutStream.toByteArray());
 			objOutStream.close();
 		}
 		catch (IOException ex) {
@@ -37,7 +40,7 @@ public class Student implements Serializable {
 			byte[] decode = Base64.getDecoder().decode(binary);
 			ByteArrayInputStream byteInStream = new ByteArrayInputStream(decode);
 			ObjectInputStream objInStream = new ObjectInputStream(byteInStream);
-			deserialStudent = (Student)objInStream.readObject();
+			deserializedBinaryStudent = (Student)objInStream.readObject();
 			objInStream.close();
 		}
 		catch (IOException ex) {
@@ -54,15 +57,36 @@ public class Student implements Serializable {
 	public static void printStudent(Student student) {
 		System.out.println("Name: " + student.firstName +
 							"\nBirth Year: " + student.birthYear +
-							"\nYear in College: " + student.yearInCollege +
+							"\nYears in College: " + student.yearsInCollege +
 							"\nCWID: " + student.cwid);
 	}
 	
-	public static void main(String[] args) {
-		Student stu = new Student("jack", "1997" , "2", "12221333");
-		serializeToBinary(stu);
-		System.out.println(binary);
-		deserializeBinary(binary);
-		printStudent(deserialStudent);
+	public static boolean serializeXML(String file, Student student) {
+		XMLEncoder encode = null;
+		try {
+			FileOutputStream fileOutStream = new FileOutputStream(file);
+			encode = new XMLEncoder(new BufferedOutputStream(fileOutStream));
+		}
+		catch (FileNotFoundException ex) {
+			System.out.println("File Entered Not Found");
+			return false;
+		}
+		encode.writeObject(student);
+		encode.close();
+		return true;
+	}
+	
+	public static boolean deserializeXML(String file) {
+		try {
+			FileInputStream fileInStream = new FileInputStream(file);
+			XMLDecoder decode = new XMLDecoder(fileInStream);
+			deserializedXMLStudent = (Student) decode.readObject();
+			decode.close();
+		}
+		catch (FileNotFoundException ex) {
+			System.out.println("File Not Found");
+			return false;
+		}
+		return true;
 	}
 }
--- StudentDriver.java
+++ StudentDriver.java
@@ -12,19 +12,26 @@ public class StudentDriver extends Student{
 		}
 		else {
 			Student otherStudent = (Student) student;
-			return firstName.equals(otherStudent.firstName) && 
-					birthYear.equals(otherStudent.birthYear) &&
-					yearInCollege.equals(otherStudent.yearInCollege) &&
-					cwid.equals(otherStudent.cwid);
+			return firstName == otherStudent.firstName && 
+				   birthYear == otherStudent.birthYear &&
+				   yearsInCollege == otherStudent.yearsInCollege &&
+				   cwid == otherStudent.cwid;
 		}
+		
 	}
 	
 	public static void main(String[] args) {
 		Student stu = new Student("jack", "1997" , "2", "12221333");
+		Student stu2 = new Student("john", "1998" , "3", "32225333");
 		serializeToBinary(stu);
-		System.out.println(binary);
-		deserializeBinary(binary);
-		printStudent(deserialStudent);
+		System.out.println(binaryNumber);
+		deserializeBinary(binaryNumber);
+		printStudent(deserializedBinaryStudent);
+		System.out.println("Are these the same student? " + stu.equals(deserializedBinaryStudent));
+		serializeXML("student.xml", stu2);
+		//deserializeXML("student.xml");
+		//System.out.println(deserializedXMLStudent);
+		printStudent(deserializedXMLStudent);
 	}
 	
 }
